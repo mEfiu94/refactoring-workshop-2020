ca90393a62dce0f3d57ec2a7ad47f30c60f698fc
Mateusz Krajnik <mateusz.krajnik@protonmail.com>
2020-10-23 17:25:01 +0200

    Add break in switch cases

--- SnakeController/SnakeController.cpp
+++ SnakeController/SnakeController.cpp
@@ -214,17 +214,21 @@ Controller::Segment Controller::getNewHead() const
 }
 
 void Controller::receive(std::unique_ptr<Event> e)
-{
+{   
     auto eventType=e->getMessageId();
     switch(eventType){
         case TimeoutInd::MESSAGE_ID:
             handleTimePassed(*static_cast<EventT<TimeoutInd> const&>(*e));
+            break;
         case DirectionInd::MESSAGE_ID:
             handleDirectionChange(*static_cast<EventT<DirectionInd> const&>(*e));
+            break;
         case FoodInd::MESSAGE_ID:
             handleFoodPositionChange(*static_cast<EventT<FoodInd> const&>(*e));
+            break;
         case FoodResp::MESSAGE_ID:
             handleNewFood(*static_cast<EventT<FoodResp> const&>(*e));
+            break;
         default:
             throw UnexpectedEventException();
     }    
